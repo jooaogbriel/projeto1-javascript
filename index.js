428ea45c122a7d7b0f6c857eca3f5e049636c705
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2022-10-25 09:37:16 -0300

    const-functions

---
+++ index.js
@@ -0,0 +1,32 @@
+let idade = 5
+
+let nome = 'joao'
+
+let gamer = true  
+
+const pessoa = {
+    nome: 'joao',
+    idade: 19,
+}
+
+let array = [ idade = 19, nome = 'joao']
+
+
+let varCor = 'azul'
+function mudaCor (cor) {
+    varCor = cor
+}
+
+console.log(idade)
+
+console.log(nome)
+
+console.log(gamer)
+
+console.log(pessoa)
+
+console.log(array[1])
+
+console.log(varCor)
+mudaCor("verde")
+console.log(varCor)
